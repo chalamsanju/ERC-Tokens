c7b93601bc9268deb278e63f17e3d1be90253f5e
chalam sanjay <127350792+chalamsanju@users.noreply.github.com>
2024-07-25 14:13:19 +0530

    Update upgradable.sol

--- upgradable.sol
+++ upgradable.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
