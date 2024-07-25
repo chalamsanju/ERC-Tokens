331988998820cd1a31db21ae4e54291d5e12cdbb
chalamsanju <chalamsanjay@gmail.com>
2024-07-25 14:07:53 +0530

    initial commit

---
+++ upgradable.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
